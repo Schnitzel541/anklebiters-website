5b9053b90faa554ec10e7e9b9ccc84340f111f77
Schnitzel541 <j.budding122@gmail.com>
2024-10-29 20:40:27 +0100

    Integrate webpack into project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src\\js\\index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development', 
+};

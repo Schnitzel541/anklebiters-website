a16bdfb92a4e2ec53989d0b96a2af0eea1b99a47
Schnitzel541 <j.budding122@gmail.com>
2024-11-04 20:43:42 +0100

    Work on merchandise page

--- src/js/merch.js
+++ src/js/merch.js
@@ -1,9 +1,57 @@
 import '../css/global.css';
+import '../css/merch.css';
+import merchFront from '../assets/merch_front.jpg'
+import merchBack from '../assets/merch_back.jpg'
 
 const mainContent = document.querySelector(".main-content");
-const content = document.createElement("h3");
+const merchPageContent = document.createElement("div")
+const leftContent = document.createElement("div");
+const rightContent = document.createElement("div");
+
+const shirtFront = document.createElement('img');
+const shirtBack = document.createElement('img');
+
+const buyButton = document.createElement('button');
+
+merchPageContent.className = "merch-page-content";
+
+shirtFront.src = merchFront;
+shirtBack.src = merchBack;
+shirtFront.className = 'shirt-img';
+shirtBack.className = 'shirt-img';
+
+leftContent.className = "left-content";
+rightContent.className = "right-content";
+
+buyButton.textContent = "Interesse? Klik hier!"
 
 export const merch = () => {
-    content.textContent = "Merch";
-    mainContent.appendChild(content);
+
+    leftContent.appendChild(shirtFront);
+    leftContent.appendChild(shirtBack);
+    
+    rightContent.innerHTML = `<div class="right-content-text"><h1>Officiëel ANKLEBITERS T-shirt</h3><br>
+                              <p>Toon je support voor ANKLEBITERS met dit exclusieve, officiële 
+                              T-shirt! Perfect voor de echte fans,
+                              dit comfortabele T-shirt met een uniek ontwerp 
+                              is een must-have voor elke garderobe</p>
+                              <p>Het T-shirt is gemaakt van 100% katoen</p>
+                              <p>Beschikbaar in de volgende maten:</p>
+                              <ul>
+                                <li>S</li>
+                                <li>M</li>
+                                <li>L</li>
+                                <li>XL</li>
+                                <li>XXL</li>
+                              </ul>
+                              <h3>€17,50</h3></div>`
+
+                          
+
+                              
+    rightContent.appendChild(buyButton);
+    merchPageContent.appendChild(leftContent);
+    merchPageContent.appendChild(rightContent);
+
+    mainContent.appendChild(merchPageContent);
 }

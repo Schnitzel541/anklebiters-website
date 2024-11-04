bdd7a95eff594bb919d650494bd2945609b957db
Schnitzel541 <j.budding122@gmail.com>
2024-11-04 07:47:01 +0100

    Add text to homepage

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -5,12 +5,16 @@ import anklebitersLogo from '../assets/anklebiters-logo-white.png'
 const logo = document.createElement("img")
 const mainContent = document.querySelector(".main-content");
 const homepageContent = document.createElement("div");
+const homepageText = document.createElement("p");
 
 export const homepage = () => {
     logo.classList.add("anklebiters-logo")
     logo.src = anklebitersLogo;
     homepageContent.classList.add("homepage-content");
     homepageContent.appendChild(logo);
-
+    homepageText.textContent = '---COMING SOON---'
+    
+    
+    homepageContent.appendChild(homepageText);
     mainContent.appendChild(homepageContent);
 }

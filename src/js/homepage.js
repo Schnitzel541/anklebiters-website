83aef5f467d9ce76e0d19ddbdaa1526a46ee113b
Schnitzel541 <j.budding122@gmail.com>
2024-11-04 07:58:33 +0100

    Add button to hpmepage

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -6,6 +6,7 @@ const logo = document.createElement("img")
 const mainContent = document.querySelector(".main-content");
 const homepageContent = document.createElement("div");
 const homepageText = document.createElement("p");
+const homepageButton = document.createElement('button');
 
 export const homepage = () => {
     logo.classList.add("anklebiters-logo")
@@ -13,8 +14,12 @@ export const homepage = () => {
     homepageContent.classList.add("homepage-content");
     homepageContent.appendChild(logo);
     homepageText.textContent = '---COMING SOON---'
-    
+
+    // button
+    homepageButton.textContent = "SHOWS";
+    homepageButton.className = 'homepage-button';
     
     homepageContent.appendChild(homepageText);
+    homepageContent.appendChild(homepageButton);
     mainContent.appendChild(homepageContent);
 }

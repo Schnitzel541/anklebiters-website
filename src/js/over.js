183ff8b86fd2ee62da196c8d267409b78b26f078
Schnitzel541 <j.budding122@gmail.com>
2024-11-05 21:28:31 +0100

    Added about me page

--- src/js/over.js
+++ src/js/over.js
@@ -1,9 +1,17 @@
 import '../css/global.css';
+import '../css/over.css';
+
+import overHeader from "/src/assets/over-header.jpg";
 
 const mainContent = document.querySelector(".main-content");
-const content = document.createElement("h3");
+const headerImage = document.createElement('img');
+const overContent = document.createElement('div');
+
+headerImage.src = overHeader;
+headerImage.className = "over-header"
+
 
 export const over = () => {
-    content.textContent = "About us";
-    mainContent.appendChild(content);
+
+    mainContent.appendChild(headerImage);
 }
